7a03d794ed9e6ef285032d6c965ad8511b3c8b82
deepvn98 <deepvn988@gmail.com>
2021-03-27 20:01:06 +0700

    case

--- display.js
+++ display.js
@@ -2,9 +2,10 @@ function disPlay( index ){
     console.log(object.quizs[ index ]);
     document.getElementById("question") . innerHTML=object.quizs[index].question;
     document.getElementById( "img" ) . src = object.quizs[index].img;
-    document.getElementById( "explain" ) . innerHTML = "Nói gì đó về câu trước: " + object.quizs[index - 1].explain;
-    for ( let i = 1; i <= object.quizs[index].answer.length; i++ ){
-        document.getElementById("ans" + i).innerHTML = object.quizs[ index ].answer[ i-1 ];
+    for ( let i = 1; i <= object.quizs[ index ].answer.length; i++ ){
+        document.getElementById("ans" + i).innerHTML = object.quizs[ index ].answer[ i - 1 ];
     }
+    document.getElementById( "explain" ) . innerHTML = "Nói gì đó về câu trước: " + object.quizs[index - 1].explain;
+
 }
 disPlay(0)
