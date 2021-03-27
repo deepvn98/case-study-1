47645faaea35ef628d61e864442925f7b53120c7
deepvn98 <deepvn988@gmail.com>
2021-03-27 19:28:54 +0700

    study

--- display.js
+++ display.js
@@ -1,9 +1,9 @@
 function disPlay( index ){
     console.log(object.quizs[ index ]);
     document.getElementById("question") . innerHTML=object.quizs[index].question;
-    document.getElementById( "img" ) . src=object.quizs[index].img;
-    // document.getElementById( "explain" ) . innerHTML = "Giải thích câu trước: " + object.quizs[index - 1].explain;
-    for ( let i = 1; i <= object.quizs[index].answer.length; i++ ){
+    document.getElementById( "img" ) . src = object.quizs[index].img;
+    document.getElementById( "explain" ) . innerHTML = "Giải thích câu trước: " + object.quizs[index - 1].explain;
+    for ( let i = 0; i < object.quizs[index].answer.length; i++ ){
         document.getElementById("ans" + i).innerHTML = object.quizs[ index ].answer[ i-1 ];
     }
 }
